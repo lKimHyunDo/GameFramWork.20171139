b225ef6271308411d84a43a649b73c05fcffbb94
k k <khd1088@naver.com>
2018-11-22 11:48:20 +0900

    PP15.FSM 11/22

--- GameFrameWork/PP15.FSM/Game.cpp
+++ GameFrameWork/PP15.FSM/Game.cpp
@@ -29,9 +29,6 @@ bool Game::init(const char * title, int xpos, int ypos, int width, int height, b
 
 	TheTextureManager::Instance()->load("assets/animate-alpha.png", "animate", m_pRenderer);
 
-	m_gameObjects.push_back(new Player(new LoaderParams(100, 100, 128, 82, "animate")));
-	m_gameObjects.push_back(new Enemy(new LoaderParams(300, 300, 128, 82, "animate")));
-
 	m_pGameStateMachine = new GameStateMachine();
 	m_pGameStateMachine->changeState(MenuState::Instance());
 
