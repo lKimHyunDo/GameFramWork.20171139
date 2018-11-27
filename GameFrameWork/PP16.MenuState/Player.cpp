1a4c3bf0e4e6c6d39a508330bcb0276c0af99f60
김현도 <20171139@vision.hoseo.edu>
2018-11-27 13:12:53 +0900

    PP17.PlayState 11/27

--- GameFrameWork/PP16.MenuState/Player.cpp
+++ GameFrameWork/PP16.MenuState/Player.cpp
@@ -3,24 +3,10 @@
 
 void Player::handleInput()
 {
-	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT))
-	{
-		m_velocity.setX(2);
-	}
-	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT))
-	{
-		m_velocity.setX(-2);
-	}
-	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_UP))
-	{
-		m_velocity.setY(-2);
-	}
-	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_DOWN))
-	{
-		m_velocity.setY(2);
-	}
+	Vector2D* target = TheInputHandler::Instance()->getMousePosition();
+	m_velocity = *target - m_position;
+	m_velocity /= 50;
 }
-
 Player::Player(const LoaderParams* pParams) : SDLGameObject(pParams)
 {
 }
@@ -35,7 +21,7 @@ void Player::update()
 	m_velocity.setX(0);
 	m_velocity.setY(0);
 	handleInput(); // add our function
-	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 5));
 	SDLGameObject::update();
 }
 void Player::clean()
